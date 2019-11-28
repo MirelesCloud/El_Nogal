4452ea75a1185d1d90b7694c482896967a27f8a8
Alex Mireles <anchises1968@gmail.com>
2019-11-27 16:37:09 -0800

    added css

--- src/App.js
+++ src/App.js
@@ -22,15 +22,15 @@ function App() {
 
   return (
     <>
-      <div className="jumbotron">
+      <div className="jumbotron" style={{background: "linear-gradient(to bottom, #00cc00 0%, #006600 100%)"}}>
         <div className="container">
           <div className="text-center">
-          <h1 className="text-uppercase">El Nogal Irrigation Meter</h1>
+          <h1 className="text-uppercase text-white">El Nogal Irrigation Meter</h1>
           </div>
         </div>
         </div>
         <div className="container">
-        <h1 >El Nogal Pump</h1>
+        <h2>El Nogal Pump</h2>
           <div className="row justify-content-center">
             <div className="col-md-4 col-sm-12 my-4">
               <Form>
@@ -50,8 +50,8 @@ function App() {
               </Form>
             </div>
             <div className="col-md-6 col-sm-12 my-4">
-              <h1>{(result).toFixed(1)} {" "} inches per acre</h1>
-              <h1>{(acreFeet).toFixed(1)} {" "} acre/feet</h1>
+              <h2>{(result).toFixed(1)} {" "} inches per acre</h2>
+              <h2>{(acreFeet).toFixed(1)} {" "} acre/feet</h2>
             </div>
           </div>
       </div>
@@ -83,40 +83,34 @@ function CanalPump() {
 
   return (
     <>
-      
-        <div className="container">
-        <h1>Canal Pump</h1>
-          <div className="row m-5">
-            <div className="col-md-4">
-              <label ><strong>MeterStart</strong></label>{" "}
-              <input type="number" value={meterStart} onChange={e => setmeterStart(e.target.value)}/>
+      <div className="container">
+        <h2>Canal Pump</h2>
+          <div className="row justify-content-center">
+            <div className="col-md-4 col-sm-12 my-4">
+              <Form>
+                <FormGroup>
+                  <Label for="meterStart">Meter Start</Label>
+                  <Input type="number" name="meterStart" placeholder="Meter Starting Value" value={meterStart} onChange={e => setmeterStart(e.target.value)}/>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="meterEnd">Meter End</Label>
+                  <Input type="number" name="meterEnd" placeholder="Meter Ending Value" value={meterEnd} onChange={e => setmeterEnd(e.target.value)} />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="acres">Acres</Label>
+                  <Input type="number" name="acres" placeholder="Acres Irrigated" value={acres} onChange={e => setAcres(e.target.value)}/>
+                </FormGroup>
+                <Button onClick={calculateTotal}>Calculate</Button>
+              </Form>
             </div>
-            <div className="col-md-4">
-              <label><strong>Meter End</strong></label>{" "}
-              <input type="number" value={meterEnd} onChange={e => setmeterEnd(e.target.value)}/>
-            </div>
-            <div className="col-md-4">
-              <label><strong>Acres</strong></label>{" "}
-              <input type="number" value={acres} onChange={e => setAcres(e.target.value)}/>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col text-center">
-              <button className="btn btn-primary" onClick={calculateTotal}>Calculate</button>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col m-3 text-center">
-              <h1>{(result).toFixed(1)} {" "} inches per acre</h1>
-              <h1>{(acreFeet).toFixed(1)} {" "} acre/feet</h1>
+            <div className="col-md-6 col-sm-12 my-4">
+              <h2>{(result).toFixed(1)} {" "} inches per acre</h2>
+              <h2>{(acreFeet).toFixed(1)} {" "} acre/feet</h2>
             </div>
           </div>
       </div>
-      
     </>
-   
   );
-
-
 }
+
 export default App;
